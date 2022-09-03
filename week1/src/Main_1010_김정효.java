16f317af94874bf0246d73b5463f94d6b7843725
hyooo-o <xozo5939@gmail.com>
2022-09-04 00:14:32 +0900

    0902(금)_BOJ_1010 -edit

--- week1/src/Main_1010_김정효.java
+++ week1/src/Main_1010_김정효.java
@@ -3,9 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 /**
- * BOJ 1010 다리놓기
+ * BOJ 1010 다리놓기 - (완료)
  * m(r)개 중에 n(c)개 선택하는 그냥 조합으로 풀면 시간 초과 발생 => 배열에 이전 결과 저장하여 반복 계산 작업을 줄여줌
  * rCc == (r-1)Cc + (r-1)C(c-1) 활용
+ * (실수)
+ * - 조건 잡는게 어려웠음..
+ * - (line 38) return 할 때, dp[r][c] =  을 안적어서 시간초과남 ㅠ
  * @author kjh
  *
  */
@@ -14,6 +17,7 @@ public class Main_1010_김정효 {
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
 		int T = Integer.parseInt(br.readLine());
 		dp = new int[31][31];	// n, m < 30
 		
@@ -21,16 +25,17 @@ public class Main_1010_김정효 {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
 			int m = Integer.parseInt(st.nextToken());
-			comb(m, n);
+			sb.append(comb(m, n)).append("\n");
 		}
+		System.out.println(sb);
 	}
 
 	private static int comb(int r, int c) {
-		if (c == 0 || c == r) return dp[r][c] = 1;		// rC0 또는 rCr 이면 1 대입
+		if (dp[r][c] > 0) return dp[r][c];
 		
-		else if (c > r/2) return dp[r][c]; 				// r/2를 기준으로 앞에 했던 연산이 뒤에 할 연산과 중복됨
+		else if (c == 0 || c == r) return dp[r][c] = 1;		// rC0 또는 rCr 이면 1 대입
 		
-		else return comb(r-1, c)+comb(r-1, c-1);		// rCc == (r-1)Cc + (r-1)C(c-1)
+		else return dp[r][c] = comb(r-1, c)+comb(r-1, c-1);		// rCc == (r-1)Cc + (r-1)C(c-1)   	// 여기서 return 할 때, dp[r][c] =  을 안적어서 시간초과남 ㅠ
 	}
 
 }
