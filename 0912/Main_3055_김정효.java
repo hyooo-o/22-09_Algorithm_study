8de97dee6821caf3cf4e3c2f39280e4308ccb09a
hyooo-o <72725382+hyooo-o@users.noreply.github.com>
2022-09-15 21:16:18 +0900

    0912 -ing

---
+++ 0912/Main_3055_김정효.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/**
+ * BOJ 3055 탈출 - (작성중)
+ * @author kjh
+ *
+ */
+public class Main_3055_김정효 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+	}
+
+}
