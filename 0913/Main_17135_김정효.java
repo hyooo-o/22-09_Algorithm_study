791ea28fff5fe73a077efb6784b3ab66ad8db797
hyooo-o <72725382+hyooo-o@users.noreply.github.com>
2022-09-18 23:50:15 +0900

    0913 -ing

---
+++ 0913/Main_17135_김정효.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/**
+ * BOJ 17135 캐슬디펜스 - 진행중
+ * @author kjh
+ *
+ */
+public class Main_17135_김정효 {
+	static int n, m, d;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		d = Integer.parseInt(st.nextToken());
+		
+		for (int i = 0; i <n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+			}
+		}
+	}
+
+}
