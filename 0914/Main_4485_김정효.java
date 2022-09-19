3b0e534525c00685d43845474725599765078c2b
hyooo-o <72725382+hyooo-o@users.noreply.github.com>
2022-09-19 23:36:44 +0900

    Create Main_4485_김정효.java

---
+++ 0914/Main_4485_김정효.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_4485_김정효 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < args.length; i++) {
+			
+		}
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+	}
+
+}
