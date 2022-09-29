2672b95f933bca114eee72ff94585913df2e24e1
KathleenJung <85939045+KathleenJung@users.noreply.github.com>
2022-09-29 21:39:03 +0900

    BOJ #1010

--- 0902/Main_1010_정유진.java
+++ 0902/Main_1010_정유진.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main_1010_정유진 {
+public class Main {
 	static int [][]arr=new int[30][30];
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
